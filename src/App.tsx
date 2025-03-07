28908da05ed013494c18af72a50102d7be02cd60
knewlin713 <kaley_newlin@brown.edu>
2025-03-07 02:52:15 -0500

    Finished, see if deploys

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { HashRouter, Routes, Route, Link } from "react-router";
 import React, { SetStateAction, useState } from "react";
 import Projects from "./pages/projects.js";
 import Home from "./pages/home.js";
@@ -21,24 +21,24 @@ function App() {
   //projects should have a projects home page, then click
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <div className="navigation">
           <nav>
             <ul>
-              <li><Link to="knewlin-CS1300">Home</Link></li>
-              <li><Link to="knewlin-CS1300/projects">Projects</Link></li>
-              <li><Link to="knewlin-CS1300/resume">Resume</Link></li>
+              <li><Link to="">Home</Link></li>
+              <li><Link to="projects">Projects</Link></li>
+              <li><Link to="resume">Resume</Link></li>
             </ul>
           </nav>
         </div>
         <Routes>
-          <Route path="knewlin-CS1300" element={<Home />} />
-          <Route path="knewlin-CS1300/projects" element={<Projects />} />
-          <Route path="knewlin-CS1300/resume" element={<Resume />} />
-          <Route path="knewlin-CS1300/projects/demo" element={<Demo />} />
-          <Route path="knewlin-CS1300/projects/demo2" element={<Demo2 />} />
+          <Route path="" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="resume" element={<Resume />} />
+          <Route path="demo" element={<Demo />} />
+          <Route path="demo2" element={<Demo2 />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
     </div>
   )
