02d21756132396445597cab8e13adaa211958dd3
knewlin713 <kaley_newlin@brown.edu>
2025-05-07 06:43:37 -0400

    link fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div style={{ height: '100vh', width: '100%' }}>
             <iframe
-                src="/mainPage.html"
+                src="./mainPage.html"
                 style={{ width: '100%', height: '100%', border: 'none' }}
             />
         </div>
